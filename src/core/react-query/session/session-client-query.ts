f4564b8058a7c7faa995c668017e1c03c508cb8f
load28 <tjalsdud89@naver.com>
2025-01-15 13:02:12 +0900

    feat: add auth session prefetch and session query hook

---
+++ src/core/react-query/session/session-client-query.ts
@@ -0,0 +1,39 @@
+import { useSuspenseQuery } from '@tanstack/react-query';
+import {
+  SESSION_QUERY_KEY,
+  SESSION_QUERY_STALE_TIME,
+  SessionInfoSchema,
+  TSessionInfo
+} from '@stock-day/core/react-query/session/session-constants';
+
+export const useSessionQuery = () => {
+  return useSuspenseQuery<TSessionInfo | null>({
+    queryKey: [SESSION_QUERY_KEY],
+    staleTime: SESSION_QUERY_STALE_TIME,
+    queryFn: async (): Promise<TSessionInfo | null> => {
+      try {
+        const response = await fetch(`${process.env.NEXT_PUBLIC_APP_URL}/api/auth/session`);
+        if (!response.ok) {
+          // throw new Error(`HTTP error status: ${response.status}`);
+          return null;
+        }
+
+        const responseBody = await response.json();
+        const { error, data: user } = SessionInfoSchema.safeParse(responseBody?.user);
+        if (error) {
+          // throw new Error(`Invalid session: ${error.message}`);
+          return null;
+        }
+
+        return {
+          id: user.id,
+          email: user.email,
+          image: user.image
+        };
+      } catch (error: unknown) {
+        // throwError(error);
+        return null;
+      }
+    }
+  });
+};
